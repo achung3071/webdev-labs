b3c57f16965b26eb0fdbf535d9ad838464fcbf61
Andrew Chung <andrewchung2022@u.northwestern.edu>
2022-05-06 05:02:55 -0500

    complete dyslexia mode

--- lab06/03-dyslexia-mode/index.js
+++ lab06/03-dyslexia-mode/index.js
@@ -2,3 +2,17 @@
   See Smashing Magazine Tutorial:
   https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
 */
+const button = document.querySelector("#dyslexia-toggle");
+const content = document.querySelector("body");
+
+let isPressed = window.localStorage.getItem("dyslexic") === "true";
+if (isPressed) {
+  document.body.className = "dyslexia-mode";
+}
+
+button.addEventListener("click", () => {
+  content.className =
+    content.className !== "dyslexia-mode" ? "dyslexia-mode" : "";
+  isPressed = !isPressed;
+  window.localStorage.setItem("dyslexic", String(isPressed));
+});
