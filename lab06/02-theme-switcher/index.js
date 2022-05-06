469f3165a918dbdaac08c923c041dffe18019ec8
Andrew Chung <andrewchung2022@u.northwestern.edu>
2022-05-06 04:47:15 -0500

    complete theme switcher

--- lab06/02-theme-switcher/index.js
+++ lab06/02-theme-switcher/index.js
@@ -5,3 +5,21 @@
     2. Modify the className property of the body tag 
        based on the button that was clicked.
 */
+
+const body = document.querySelector("body");
+
+document.querySelector("#default").addEventListener("click", () => {
+  body.className = "";
+});
+
+document.querySelector("#ocean").addEventListener("click", () => {
+  body.className = "ocean";
+});
+
+document.querySelector("#desert").addEventListener("click", () => {
+  body.className = "desert";
+});
+
+document.querySelector("#high-contrast").addEventListener("click", () => {
+  body.className = "high-contrast";
+});
